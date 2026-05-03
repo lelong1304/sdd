47095173dae9ed0875d294eb4c4ee8070b874f98
lelong1304 <lelong1304@gmail.com>
2026-05-03 23:41:56 +0200

    first commit

---
+++ src/test/java/voxxed_luxembourg/sdd/SddApplicationTests.java
@@ -0,0 +1,13 @@
+package voxxed_luxembourg.sdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
